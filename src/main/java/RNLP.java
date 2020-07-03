af495ca361128a12922a88b451d1aa48fe5093ba
notweens <maxkatzmann20@gmail.com>
2020-07-03 21:56:01 +0300

    03-07-20

--- src/main/java/RNLP.java
+++ src/main/java/RNLP.java
@@ -1,17 +1,17 @@
+import edu.stanford.nlp.ie.crf.CRFClassifier;
 import edu.stanford.nlp.international.russian.process.RussianLemmatizationAnnotator;
 import edu.stanford.nlp.international.russian.process.RussianMorphoAnnotator;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.DependencyParseAnnotator;
-import edu.stanford.nlp.pipeline.POSTaggerAnnotator;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.ling.tokensregex.*;
+import edu.stanford.nlp.pipeline.*;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import edu.stanford.nlp.util.CoreMap;
 
 import java.io.File;
 import java.util.List;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 public class RNLP {
     private static StanfordCoreNLP pipeline;
@@ -19,7 +19,7 @@ public class RNLP {
     public static void main(String[] args) {
         String basePath = new File("").getAbsolutePath();
         Properties props = new Properties();
-        props.setProperty("annotators", "tokenize, ssplit");
+        props.setProperty("annotators", "tokenize, ssplit, pos");
         pipeline = new StanfordCoreNLP(props);
         Properties parseProps = new Properties();
         parseProps.setProperty("model", basePath + "\\src\\main\\nndep.rus.modelMFWiki100HS400_80.txt.gz");
@@ -28,20 +28,29 @@ public class RNLP {
         pipeline.addAnnotator(new RussianMorphoAnnotator());
         pipeline.addAnnotator(new RussianLemmatizationAnnotator());
         pipeline.addAnnotator(new POSTaggerAnnotator(new MaxentTagger(basePath + "\\src\\main\\russian-ud-pos.tagger")));
+        pipeline.addAnnotator(new TokensRegexAnnotator(basePath + "\\src\\main\\example.txt"));
     }
 
     public void analyzeText(List<String> text) {
+        Env env = TokenSequencePattern.getNewEnv();
+        env.setDefaultStringMatchFlags(NodePattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+        env.setDefaultStringPatternFlags(Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+        CoreMapExpressionExtractor extractor = CoreMapExpressionExtractor.createExtractorFromFile(env, new File("").getAbsolutePath() + "\\src\\main\\example.rules");
         for (String line : text) {
             if (!line.isEmpty()) {
                 Annotation annotation = pipeline.process(line);
                 List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
                 for (CoreMap sentence : sentences) {
+                    List<MatchedExpression> matchedExpressions = extractor.extractExpressions(sentence);
                     for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-                        String word = token.get(CoreAnnotations.TextAnnotation.class);
-                        String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
-                        String lem = token.get(CoreAnnotations.LemmaAnnotation.class);
-                        String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
-                        System.out.println(String.format("Print: word: [%s] pos: [%s] lem: [%s] ne: [%s]", word, pos, lem, ne));
+                        String word = token.word();
+                        String pos = token.tag();
+                        String lem = token.lemma();
+                        String ne = token.ner();
+                        System.out.println(String.format("Print: word: [%s] pos: [%s] lem: [%s] ne: [%s] (%s)", word, pos, lem, ne == null ? "0" : ne, token.get(CoreAnnotations.CoNLLUFeats.class)));
+                    }
+                    for (MatchedExpression me : matchedExpressions) {
+                        System.out.println("matched expression: " + me.getText());
                     }
                 }
             }
